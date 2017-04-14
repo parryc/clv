ca7930ea3b156c65c7287e2a88f2206d6efe69f5
Parry Cadwallader <parrycadwallader@gmail.com>
2017-04-13 21:57:14 -0700

    Add ability to edit existing def
    Requires combination of word and language, in case of homographs.

--- clv.py
+++ clv.py
@@ -60,6 +60,59 @@ def add(config, word, lang, definition, add_defintion):
 
 
 @cli.command()
+@click.argument('word')
+@click.argument('lang')
+@click.argument('definition')
+@click.option('--def_id', help='Number of definition to edit.', default=-1)
+@click.option('-a', help='Add an additional definition,\
+                          otherwise the main definition will be overwritten',
+                          is_flag=True)
+@pass_config
+def edit(config, word, lang, definition, def_id, a):
+  """Edit a word in the clvdb."""
+  echo   = click.echo
+  output = config.output
+  data   = config.data
+  rec_id = -1
+
+  if a and def_id != -1:
+    echo('Cannot add and edit an entry at the same time.')
+    return False
+
+  if def_id <= 0:
+    echo('Must select a valid definition number.')
+    return False
+
+  for idx, d in enumerate(data):
+    if d['word'] == word and d['lang'] == lang:
+      rec_id = idx
+
+  if rec_id == -1:
+    echo('Could not find entry.')
+    return False
+
+  if config.verbose:
+    echo('Before:')
+    echo(json.dumps(data[rec_id]))
+
+  if a:
+    data[rec_id]['definitions'].append(definition)
+  else:
+    try:
+      # 1 indexed to match the terminal output
+      data[rec_id]['definitions'][def_id - 1] = definition
+    except:
+      echo('{} only has {} definitions.'.format(data[rec_id]['word'], len(data[rec_id]['definitions'])))
+      return False
+
+  echo('Successfully edited {}!'.format(word))
+  if config.verbose:
+    echo('After:')
+    echo(json.dumps(data[rec_id]))
+  echo(json.dumps(data), file=output)
+
+
+@cli.command()
 @click.argument('lang', required=False)
 @pass_config
 def list(config, lang):
